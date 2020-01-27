8a1e9c30578ee4b6ce741556ecc3a42fcba9acd2
Oliver Feher <olcsee@gmail.com>
2020-01-27 07:28:12 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,14 @@
 # Your code goes here!
+class Dog
+  def bark
+    puts "woof!"
+  end
+  
+  def name=(dogs_name)
+    @this_dog_name = dogs_name
+  end
+  
+  def name
+    @this_dog_name
+  end
+end
